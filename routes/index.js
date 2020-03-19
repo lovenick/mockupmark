ce391243d9636e5e09c67038f35496409993e3f9
Ken Van den Enden <ken@enden.be>
2020-03-19 12:31:21 +0100

    Clean up leftover files.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { MOCKUPS }  = require("../services/mockups");
-const images =  MOCKUPS.map(x => x.template.split('/').slice(-2).join('/'));
-console.log(images)
-
+const { MOCKUPS } = require("../services/mockups");
+const images = MOCKUPS.map(x =>
+  x.template
+    .split("/")
+    .slice(-2)
+    .join("/")
+);
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Mockup Mark", images});
+  res.render("index", { title: "Mockup Mark", images });
 });
 
 module.exports = router;
