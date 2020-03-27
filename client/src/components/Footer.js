fb9601073f4dd1fd4283b9858ed001402f5cb9cc
Ken Van den Enden <ken@enden.be>
2020-03-27 14:15:18 +0100

    Add feedback link.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,11 +4,27 @@ import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="container">
-        <p>&copy; 2020 Mockup Mark</p>
-      </div>
-    </footer>
+    <div style={{ marginTop: "1em" }}>
+      <p className="text-center">
+        Help us out by letting us know what you think of this prototype
+        <br />
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLSdmJy9iXNQBPWci3W1U8kZ-S3yBDZ2vPtSdu-NhI2c5rCCxXg/viewform?usp=sf_link"
+          target="_blank"
+        >
+          Link to survey
+        </a>{" "}
+        or email us at{" "}
+        <a href="mailto:hello@mockupmark.com" target="_blank">
+          hello@mockupmark.com
+        </a>
+      </p>
+      <footer>
+        <div className="container">
+          <p>&copy; 2020 Mockup Mark</p>
+        </div>
+      </footer>
+    </div>
   );
 };
 
