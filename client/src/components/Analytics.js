2719111379de3311aad8ee3822107df14b0a339f
Ken Van den Enden <ken@enden.be>
2020-04-01 15:23:23 +0200

    Add facebook pixel.

--- client/src/components/Analytics.js
+++ client/src/components/Analytics.js
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
 import ReactGA from "react-ga";
 import FullStory from "react-fullstory";
+import FacebookPixel from "react-facebook-pixel";
 
 const Analytics = () => {
   useEffect(() => {
     ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID);
     ReactGA.pageview(window.location.pathname + window.location.search);
+
+    FacebookPixel.init(process.env.REACT_APP_FACEBOOK_PIXEL_ID);
+    FacebookPixel.pageView();
   }, []);
 
   return <FullStory org={process.env.REACT_APP_FULLSTORY_ORG_ID} />;
